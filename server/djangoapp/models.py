011801c0142562be16502e2aeeac4ddfaea3b6f2
Aashiq-N <aashiq.mymail@gmail.com>
2025-07-06 18:33:28 -0400

    Flake8 for .py files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,11 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -15,23 +15,31 @@ class CarMake(models.Model):
         return self.name
 
 
-
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Many-to-One
     name = models.CharField(max_length=100)
-    
+
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
-    year = models.IntegerField(default=2023, validators=[
-        MaxValueValidator(2023),
-        MinValueValidator(2015)
-    ])
-    
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default="SUV"
+    )
+
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015),
+        ],
+    )
+
     dealer_id = models.IntegerField(default=1)
 
     def __str__(self):
